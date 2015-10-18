b068538e46f360bcf0f739d58c61c30bfcf9260c
reality <tinmachin3@gmail.com>
2015-10-18 20:37:05 +0000

    add note

--- forwards.groovy
+++ forwards.groovy
@@ -8,18 +8,19 @@ def toEntail = [ "Girl" ]
 def entailed = []
 
 // Naive 
+// TODO: https://en.wikipedia.org/wiki/Unit_propagation
 while(toEntail.size() > 0) {
   KB.each { clause ->
     def positive = clause.last() // Positive literal is last
     if(entailed.contains(positive)) return;
 
     if(clause.size() == 1) {
-      entailed.push(positive)
+      entailed << positive
       toEntail -= positive
     } else {
       def ent = clause.every { entailed.contains(it) || it == positive }
       if(ent) {
-        entailed.push(positive)
+        entailed << positive
         toEntail -= positive
       }
     }
