cd64c430a5837c0e8fcd91526b0d15f26ed9cbd0
reality <tinmachin3@gmail.com>
2015-10-18 20:05:19 +0000

    naive implementation of forward chaining

---
+++ forwards.groovy
@@ -0,0 +1,31 @@
+import groovy.json.JsonSlurper
+
+// Load the KB
+def file = new File('knowledgebase.json')
+def KB = new JsonSlurper().parse(file).clauses
+
+def toEntail = [ "Girl" ]
+def entailed = []
+
+// Naive O(kb^2)
+while(toEntail.size() > 0) {
+  KB.each { clause ->
+    def positive = clause.last() // Positive literal is last
+    if(entailed.contains(positive)) return;
+
+    if(clause.size() == 1) {
+      entailed.push(positive)
+      toEntail -= positive
+    } else {
+      def ent = clause.every { entailed.contains(it) || it == positive }
+      if(ent) {
+        entailed.push(positive)
+        toEntail -= positive
+      }
+    }
+  }
+
+  println "Round complete"
+}
+
+println "KB ⊨ Girl"
